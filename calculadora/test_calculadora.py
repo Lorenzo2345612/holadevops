2fd364457c819f4a75651c749a554eec1f952107
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2025-05-07 13:20:44 -0600

    Hola

---
+++ calculadora/test_calculadora.py
@@ -0,0 +1,14 @@
+import unittest
+from calculadora import Calculadora
+
+
+class TestCalculadora(unittest.TestCase):
+
+    def test_sumar_dos_mas_dos(self):
+        cal = Calculadora()
+        resultado = cal.sumar(2, 2)
+        self.assertEqual(resultado, 4, "La suma de 2 + 2 debería ser 4")
+
+
+if __name__ == '__main__':
+    unittest.main()
